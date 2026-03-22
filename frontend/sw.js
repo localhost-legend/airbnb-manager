6f13283195f0391e2714324324bff0794636b5d5
Shubham Makkar <13jerry02@gmail.com>
2026-03-22 16:42:01 +0530

    fix: add PWA support

---
+++ frontend/sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', (event) => {event.waitUntil(caches.open('airbnb-manager').then((cache) => cache.addAll(['/index.html', '/app.js', '/style.css'])))});
+self.addEventListener('fetch', (event) => {event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)))});
